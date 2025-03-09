5f85153faa52aab2ecb466146e3beff0b8eda1d7
Zaur <zaurmamedov339933@gmail.com>
2025-03-09 23:30:32 +0200

    fixafterreview

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,17 +5,19 @@
  */
 function calculateRentalCost(days) {
   const price = days * 40;
+  const threeDaysDiscount = 20;
+  const sevenDaysDiscount = 50;
 
   if (days < 3) {
     return price;
   }
 
   if (days >= 3 && days < 7) {
-    return price - 20;
+    return price - threeDaysDiscount;
   }
 
   if (days >= 7) {
-    return price - 50;
+    return price - sevenDaysDiscount;
   }
 }
 
